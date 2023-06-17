665a6ddd57615cbf83d60d9e2b6be06eff393743
Frank Sarfo <sarfof06@gmail.com>
2023-06-16 22:37:37 -0700

    My first Code  in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with put function
+ * Return: 0 (When it return 0, it means Success)
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
