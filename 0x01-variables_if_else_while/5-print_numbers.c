b38930bb1d82a2406cd779361ab0114160f4e037
Frank Sarfo <sarfof06@gmail.com>
2023-06-26 21:49:27 -0700

    1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints numbers for 0 to 9
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf(i);
+i++;
+printf('\n');
+return (0);
+}
+}
