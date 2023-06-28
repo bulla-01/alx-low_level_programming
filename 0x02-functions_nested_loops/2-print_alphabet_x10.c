64fbee1636222ec0f380f51c59750ecbfaac7e09
Frank Sarfo <sarfof06@gmail.com>
2023-06-28 13:27:21 -0700

    1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - prints alphabets 10 times
+ * Return: Always (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+char l;
+int i =1;
+
+while (i <= 10)
+{
+ for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+i++;
+}
+}
