7c0ec6393525214f05772114bbf7322dcd13f8d7
Frank Sarfo <sarfof06@gmail.com>
2023-06-28 03:50:37 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints _putchar
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char a = "_putchar";
+putchar(a);
+putchar('\n');
+}
