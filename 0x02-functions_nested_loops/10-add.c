c6f1a955c2888fdacedb31a0797516ce51752625
Frank Sarfo <sarfof06@gmail.com>
2023-06-30 08:21:14 -0700

    1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - A function that add two integers
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Always (Success)
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
