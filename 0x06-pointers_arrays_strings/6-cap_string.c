fe7cc930124cda4d1dfcc9c2d6f0323530e709ca
Frank Sarfo <sarfof06@gmail.com>
2023-06-29 18:05:32 -0700

    3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * cap_string - Capitalizes all words of string
+ * *cap_string - Capitalizes all words of string
  * @str: The string to be capitalized
  * Return: A pointer to change string
  */
